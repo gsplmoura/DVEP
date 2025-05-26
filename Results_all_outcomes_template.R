8b168e6a6389e212b1af656510747c95f4f99473
gsplmoura <moura-gustavo@hotmail.com>
2025-05-26 10:27:23 -0300

    25-05-26

--- Results_all_outcomes_template.R
+++ Results_all_outcomes_template.R
@@ -1,83 +1,83 @@
-### labs_ggt
+### labs_alkp
 
-```{r labs_ggt_1}
+```{r labs_alkp_1}
 # Plot 1: Raw data
-labs_ggt_hist_1 <- data_model %>% 
+labs_alkp_hist_1 <- data_model %>% 
     #filter(
-    #    labs_ggt < 300
+    #    labs_alkp < 300
     #) %>% 
-    ggplot(aes(x = labs_ggt)) + 
+    ggplot(aes(x = labs_alkp)) + 
     geom_histogram(bins = 50, fill = "skyblue", color = "black")
 
 # Plot 2: Log-transformed data
-labs_ggt_hist_2 <- data_model %>% 
+labs_alkp_hist_2 <- data_model %>% 
     #filter(
-    #    labs_ggt < 300
+    #    labs_alkp < 300
     #) %>%
-    ggplot(aes(x = log1p(labs_ggt))) + 
+    ggplot(aes(x = log1p(labs_alkp))) + 
     geom_histogram(bins = 50, fill = "lightgreen", color = "black")
 
 # Combine side by side
-labs_ggt_hist_1 + labs_ggt_hist_2 # library(patchwork)
+labs_alkp_hist_1 + labs_alkp_hist_2 # library(patchwork)
 ```
 
-```{r labs_ggt_2}
+```{r labs_alkp_2}
 # LMM
-labs_ggt_model <- lmer(log1p(labs_ggt) ~ allocation_group + visit + (1 | record_id), data = data_model)
-check_collinearity(labs_ggt_model)
+labs_alkp_model <- lmer(log1p(labs_alkp) ~ allocation_group + visit + (1 | record_id), data = data_model)
+check_collinearity(labs_alkp_model)
 
 # Sensitivity analysis
-labs_ggt_model_check <- sensitivity_check_lmer(
-    model = labs_ggt_model,
+labs_alkp_model_check <- sensitivity_check_lmer(
+    model = labs_alkp_model,
     id_var = "record_id",
     top_n = 5)
 
 # LMM Sensitivity
-labs_ggt_model_sens <- update(object = labs_ggt_model,
-                              subset = !(record_id %in% labs_ggt_model_check$influential_ids))
+labs_alkp_model_sens <- update(object = labs_alkp_model,
+                              subset = !(record_id %in% labs_alkp_model_check$influential_ids))
 # Influential IDS
-labs_ggt_model_check$influential_ids
+labs_alkp_model_check$influential_ids
 ```
 
-```{r labs_ggt_3}
+```{r labs_alkp_3}
 # Model comparison
-summary(labs_ggt_model)
-summary(labs_ggt_model_sens)
-labs_ggt_model_check$comparison_table
-performance::compare_performance(labs_ggt_model, labs_ggt_model_sens)
+summary(labs_alkp_model)
+summary(labs_alkp_model_sens)
+labs_alkp_model_check$comparison_table
+performance::compare_performance(labs_alkp_model, labs_alkp_model_sens)
 ```
 
-```{r labs_ggt_4, fig.width=10, fig.height=10}
-performance::check_model(labs_ggt_model)
-performance::check_model(labs_ggt_model_sens)
+```{r labs_alkp_4, fig.width=10, fig.height=10}
+performance::check_model(labs_alkp_model)
+performance::check_model(labs_alkp_model_sens)
 ```
 
-```{r labs_ggt_5}
+```{r labs_alkp_5}
 # Get EMMs for each group at each visit
-labs_ggt_emm <- emmeans::emmeans(
-    labs_ggt_model_sens, 
+labs_alkp_emm <- emmeans::emmeans(
+    labs_alkp_model_sens, 
     ~ allocation_group * visit
 )
 
 # Table of marginal means
-labs_ggt_emm
+labs_alkp_emm
 
 # Pairwise comparisons: Between groups at each visit
-contrast(labs_ggt_emm, method = "pairwise", by = "visit", adjust = "bonferroni")
+contrast(labs_alkp_emm, method = "pairwise", by = "visit", adjust = "bonferroni")
 
 # Pairwise comparisons: Changes over time within each group
-contrast(labs_ggt_emm, method = "pairwise", by = "allocation_group", adjust = "bonferroni")
+contrast(labs_alkp_emm, method = "pairwise", by = "allocation_group", adjust = "bonferroni")
 
 # Plot of marginal means
-plot(labs_ggt_emm, comparisons = TRUE)
+plot(labs_alkp_emm, comparisons = TRUE)
 ```
 
-```{r labs_ggt_6}
+```{r labs_alkp_6}
 ggplot(
     data = data_model, 
     aes(
         x = as.factor(visit),
-        y = labs_ggt,
+        y = labs_alkp,
         group = record_id,
     )
 ) +
@@ -95,12 +95,12 @@ ggplot(
 
 data_model %>% 
     filter(
-        !(record_id %in% labs_ggt_model_check$influential_ids)
+        !(record_id %in% labs_alkp_model_check$influential_ids)
     ) %>% 
     ggplot(
         aes(
             x = as.factor(visit),
-            y = labs_ggt,
+            y = labs_alkp,
             group = record_id,
         )
     ) +
